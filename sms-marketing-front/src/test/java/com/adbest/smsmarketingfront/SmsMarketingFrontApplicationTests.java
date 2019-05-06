18bcedac11f1850c85ac5f46b09dd56568cd8775
midakun <990483811@qq.com>
2019-05-06 10:35:49 +0800

    resolve package problem

--- sms-marketing-front/src/test/java/com/adbest/smsmarketingfront/SmsMarketingFrontApplicationTests.java
+++ sms-marketing-front/src/test/java/com/adbest/smsmarketingfront/SmsMarketingFrontApplicationTests.java
@@ -1,8 +1,10 @@
 package com.adbest.smsmarketingfront;
 
 import com.adbest.smsmarketingentity.MessageRecord;
+import com.adbest.smsmarketingentity.QContacts;
 import com.adbest.smsmarketingfront.util.twilio.PreSendMsg;
 import com.adbest.smsmarketingfront.util.twilio.TwilioUtil;
+import com.querydsl.core.BooleanBuilder;
 import com.twilio.rest.api.v2010.account.Message;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,14 +22,17 @@ public class SmsMarketingFrontApplicationTests {
 
     @Test
     public void contextLoads() {
-        PreSendMsg preSendMsg = new PreSendMsg();
-        MessageRecord record = new MessageRecord();
-        record.setContactsNumber("+8615669052722");
-        record.setCustomerNumber("+16782758458");
-        record.setContent("From twilio, Use Test Credentials -- test message");
-        preSendMsg.setRecord(record);
-        Message message = twilioUtil.sendMessage(preSendMsg);
-        System.out.println(message);
+        QContacts qContacts = QContacts.contacts;
+        BooleanBuilder builder = new BooleanBuilder();
+        builder.and(qContacts.customerId.eq(2L));
+//        PreSendMsg preSendMsg = new PreSendMsg();
+//        MessageRecord record = new MessageRecord();
+//        record.setContactsNumber("+8615669052722");
+//        record.setCustomerNumber("+16782758458");
+//        record.setContent("From twilio, Use Test Credentials -- test message");
+//        preSendMsg.setRecord(record);
+//        Message message = twilioUtil.sendMessage(preSendMsg);
+//        System.out.println(message);
     }
 
 }
