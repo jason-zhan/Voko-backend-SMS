9c313d5014564645fd152a2588f003b4411cf999
midakun <990483811@qq.com>
2019-05-28 17:10:02 +0800

    update for daily

--- sms-marketing-front/src/test/java/com/adbest/smsmarketingfront/InboxApplicationTests.java
+++ sms-marketing-front/src/test/java/com/adbest/smsmarketingfront/InboxApplicationTests.java
@@ -1,43 +1,29 @@
 package com.adbest.smsmarketingfront;
 
-import com.adbest.smsmarketingentity.ContactsGroup;
-import com.adbest.smsmarketingentity.Keyword;
-import com.adbest.smsmarketingentity.MessageRecord;
-import com.adbest.smsmarketingentity.ServiceNumber;
 import com.adbest.smsmarketingfront.service.ContactsGroupService;
 import com.adbest.smsmarketingfront.service.KeywordService;
 import com.adbest.smsmarketingfront.service.MessageRecordService;
-import com.adbest.smsmarketingfront.util.PageBase;
-import com.adbest.smsmarketingfront.util.twilio.param.InboundMsg;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.UUID;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class InboxApplicationTests {
-
+    
     @Autowired
     private MessageRecordService messageRecordService;
-
+    
     @Autowired
     private KeywordService keywordService;
-
+    
     @Autowired
     private ContactsGroupService contactsGroupService;
     
     @Test
-    public void test(){
+    public void test() {
 //        InboundMsg inboundMsg = new InboundMsg();
 //        inboundMsg.setMessageSid(UUID.randomUUID().toString());
 //        inboundMsg.setBody("优惠");
