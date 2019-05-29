e5303b44b50e030a0ed6a0dcc19ea30ef0a7def8
gh <m_gaohao@163.com>
2019-05-29 16:20:14 +0800

    密码配置

--- sms-marketing-entity/src/main/java/com/adbest/smsmarketingentity/Customer.java
+++ sms-marketing-entity/src/main/java/com/adbest/smsmarketingentity/Customer.java
@@ -46,8 +46,9 @@ public class Customer implements Serializable {
     }
 
     public static boolean checkPassword(String password) {
-        Pattern pattern = Pattern.compile("^[A-Z0-9]{5,25}$", Pattern.CASE_INSENSITIVE);
+        Pattern pattern = Pattern.compile("^(?=.*[0-9])(?=.*[a-zA-Z])(.{8,20})$", Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(password);
         return matcher.matches();
     }
+
 }
