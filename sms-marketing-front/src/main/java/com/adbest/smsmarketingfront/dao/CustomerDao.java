715779bb8c447f9563b1a7d78d2a3debce1a74a9
midakun <990483811@qq.com>
2019-06-14 14:41:45 +0800

    drop CreditBill class, maintain customer#credit field

--- sms-marketing-front/src/main/java/com/adbest/smsmarketingfront/dao/CustomerDao.java
+++ sms-marketing-front/src/main/java/com/adbest/smsmarketingfront/dao/CustomerDao.java
@@ -4,16 +4,26 @@ import com.adbest.smsmarketingentity.Customer;
 import com.adbest.smsmarketingfront.entity.dto.CustomerDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.util.List;
 
-public interface CustomerDao extends JpaRepository<Customer, Long> , JpaSpecificationExecutor<Customer> {
+public interface CustomerDao extends JpaRepository<Customer, Long>, JpaSpecificationExecutor<Customer> {
+    
+    @Transactional
+    @Modifying
+    @Query("update Customer set credit = credit+?1 where id = ?2 and (credit+?1)>=0")
+    int updateCreditByCustomerIdAndCredit(BigDecimal cost, Long customerId);
+    
     Customer findFirstByEmailAndPassword(String username, String encrypt);
-
+    
     Customer findByEmail(String s);
-
+    
     Customer findFirstByEmail(String email);
-
+    
     List<Customer> findByEmailIn(List<String> emails);
 }
---
+++ sms-marketing-front/src/test/java/com/adbest/smsmarketingfront/service/JpaUpdateMultiThreadTest.java
@@ -0,0 +1,65 @@
+package com.adbest.smsmarketingfront.service;
+
+import com.adbest.smsmarketingfront.dao.CustomerDao;
+import com.adbest.smsmarketingfront.util.EasyTime;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.Assert;
+
+import java.math.BigDecimal;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JpaUpdateMultiThreadTest {
+    
+    @Autowired
+    CustomerDao customerDao;
+    
+    @Test
+    public void startTest() {
+        for (int i = 0; i < 1000; i++) {
+            new Thread(new execJpaThread(i, i % 2 == 0)).start();
+        }
+        try {
+            Thread.sleep(100 * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public class execJpaThread implements Runnable {
+        
+        private int num;  // 线程编号
+        private boolean symbol;  // 符号(true: >0)
+        
+        public execJpaThread(int num, boolean symbol) {
+            this.num = num;
+            this.symbol = symbol;
+        }
+        
+        @Override
+        public void run() {
+            long start = EasyTime.nowMillis();
+            System.out.printf("%s exec[%s] >%n", num, start);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            int result = customerDao.updateCreditByCustomerIdAndCredit(BigDecimal.valueOf(symbol ? 5 : -6), 1L);
+            System.out.printf("%s x %s%n", num, result);
+            printStatus();
+            long end = EasyTime.nowMillis();
+            System.out.printf("%s over[%s] <%n", num, end);
+        }
+        
+        private void printStatus() {
+            BigDecimal credit = customerDao.findById(1L).get().getCredit();
+            Assert.isTrue(credit.compareTo(BigDecimal.ZERO) >= 0, "credit < 0");
+            System.out.printf("%s = %s%n", num, credit.toPlainString());
+        }
+    }
+}
