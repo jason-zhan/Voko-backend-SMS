92f673be2496a97318bd67c089df8c60fdd12ffb
midakun <990483811@qq.com>
2019-06-18 11:27:33 +0800

    additional finance bill component impl

---
+++ sms-marketing-front/src/main/java/com/adbest/smsmarketingfront/service/impl/FinanceBillComponentImpl.java
@@ -0,0 +1,22 @@
+package com.adbest.smsmarketingfront.service.impl;
+
+import com.adbest.smsmarketingfront.dao.FinanceBillDao;
+import com.adbest.smsmarketingfront.service.FinanceBillComponent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+@Slf4j
+public class FinanceBillComponentImpl implements FinanceBillComponent {
+    
+    @Autowired
+    FinanceBillDao financeBillDao;
+    
+    @Override
+    public int saveFinanceBill(BigDecimal amount, String description) {
+        return 0;
+    }
+}
