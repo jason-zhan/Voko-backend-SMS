248850c0b44de0173d68a85149ef2f41886ecfd6
gh <m_gaohao@163.com>
2019-07-23 17:42:47 +0800

    voko支付修改

--- sms-marketing-front/src/main/java/com/adbest/smsmarketingfront/util/VokophonePayUtils.java
+++ sms-marketing-front/src/main/java/com/adbest/smsmarketingfront/util/VokophonePayUtils.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
+import java.io.UnsupportedEncodingException;
 import java.math.BigDecimal;
+import java.net.URLEncoder;
 
 @Component
 public class VokophonePayUtils {
@@ -20,10 +22,14 @@ public class VokophonePayUtils {
 
     public VokoPayVo pay(String customer_login, String comment, BigDecimal amount) {
         StringBuffer url = new StringBuffer(environment.getProperty("vokoPayUrl")).append(vokoPayUrl);
-        url.append("&visible_comment=").append(comment).append("&internal_comment=").append(comment).append("&customer_login=").append(customer_login)
+        url.append("&visible_comment=").append(URLEncoder.encode(comment)).append("&internal_comment=").append(URLEncoder.encode(comment)).append("&customer_login=").append(customer_login)
                 .append("&amount=").append(amount.toString());
         String str = HttpTools.get(url.toString());
         VokoPayVo vokoPayVo = jsonTools.parse(str, VokoPayVo.class);
         return vokoPayVo;
+    }
+
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        System.out.println(URLEncoder.encode("asd  das","utf-8"));
     }
 }
